a416c7780d697e4acfbe06929064e8adb949fe0c
Simon125q <sz.omiecinski@gmail.com>
2025-07-13 13:12:03 +0200

    added go specific macros

---
+++ ftplugin/go.lua
@@ -0,0 +1,8 @@
+-------------------
+-- CUSTOM MACROS --
+-------------------
+
+-- log var
+vim.fn.setreg("l", 'yiwoslog.Debug("", "jjpa", jjpa)jjmm?^func\rwyw\'mf"aIn func \0180\027:nohlsearch\027')
+-- return err
+vim.fn.setreg("e", 'oif err != nil {\rreturn fmt.Errorf("%w", err)\r}jj')
